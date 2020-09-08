879d34d03b6766c9bab4ace4e9ed4ae77a7911af
plz09 <62223358+plz09@users.noreply.github.com>
2020-09-08 16:00:26 +0100

    Validation of data from the entry form
    Validation of data from the entry form

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.initChat = function(application, req, res) {
+    var dataForm = req.body;
+
+    req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+    var err = req.validationErrors();
+
+    if(err) {
+        res.render("index", {validation : err});
+        return;
+    }
+
     res.render("chat");
 }
