9bbeda5ec02735ed163aaccdb89cc71e49b28529
plz09 <62223358+plz09@users.noreply.github.com>
2020-09-11 21:51:51 +0100

    Socket.io integration with server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require('./config/server');
 
-app.listen(80, function() {
+var server = app.listen(80, function() {
     console.log('Server online');
 })
+
+require('socket.io').listen(server);
